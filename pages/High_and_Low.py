f3c05b4b67d42aceefd59bdc2281c504dbe5aa3c
tatsu_tattsu <eunsec96@gmail.com>
2025-10-09 18:41:19 +0900

    high_and_lowの更新

--- pages/High_and_Low.py
+++ pages/High_and_Low.py
@@ -9,40 +9,44 @@ st.write("" \
 "・カードは１〜９までの数字が２枚ずつあります" \
 "・どのカードも引く確率は同じです")
 
-f_number = 0
-b_number = 0
-judge = 0
-f_number = rd.randint(1 , 10)
-b_number = rd.randint(1 , 10)
-st.write(f"１枚目の数字：{f_number}")
-if f_number > b_number:
-    judge = 0
-elif f_number == b_number:
-    judge = 1
-else:
-    judge = 2
-next1 = False
-statu = 0
-if st.button("High"):
-    statu = 1
-    next1 = True
-if st.button("Low"):
-    statu = 2
-    next1 = True
-match(statu):
-    case 1:
+c = True
+def judge(a ,b):
+    if a < b:
+        return 0
+    elif a > b:
+        return 1
+    else:
+        return 2
+
+def winlose(a, b):
+    if a == 2:
+        return st.info("Draw")
+    elif a == b:
+        return st.success("You Win")
+    else:
+        return st.error("You Lose")
+
+def highandlow():
+    f_number = 0
+    b_number = 0
+    judged = 0
+    f_number = rd.randint(1 , 10)
+    b_number = rd.randint(1 , 10)
+    st.write(f"１枚目の数字：{f_number}")
+    judged = judge(f_number, b_number)
+    statu = 0
+    if st.button("High"):
+        statu = 1
         st.write(f"２枚目の数字：{b_number}")
-        if judge == 0:
-            st.error("You Lose")
-        elif judge == 1:
-            st.info("Draw")
-        else:
-            st.success("You Win")
-    case 2:
+        winlose(judged, statu)
+        if st.button("reset"):
+            statu = 0
+    elif st.button("Low"):
+        statu = 2
         st.write(f"２枚目の数字：{b_number}")
-        if judge == 2:
-            st.error("You Lose")
-        elif judge == 1:
-            st.info("Draw")
-        else:
-            st.success("You Win")
+        winlose(judged, statu)
+        st.button("reset")
+        if st.button("reset"):
+            statu = 0
+
+highandlow()
